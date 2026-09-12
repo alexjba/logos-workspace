26b540432d41e8dcace1e9ed0bf6470f7110dc3c
Alex Jbanca <alexjb@status.im>
2026-09-12 17:07:57 +0300

    chore(workspace): re-pin logos-basecamp after the Web container refactor (#17)

--- flake.nix
+++ flake.nix
@@ -174,7 +174,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     logos-basecamp = {
-      url = "github:logos-fleet/logos-basecamp/89fd5600424b3d56e97768655dbb360f48795631";
+      url = "github:logos-fleet/logos-basecamp/db6b9e042cf716eb388df2b7daa99b2b11f450ff";
       inputs.logos-capability-module.follows = "logos-capability-module";
       inputs.logos-chat-module.follows = "logos-chat-module";
       inputs.logos-cpp-sdk.follows = "logos-cpp-sdk";
